b2dd051dafd19be49a5825e2f6615815aeafc644
Jorge Londoño <jorge.londono@upb.edu.co>
2023-02-01 16:45:52 -0500

    Ejemplos ADTs

---
+++ src/ea01_ADT/Fecha.js
@@ -0,0 +1,17 @@
+class Fecha {
+
+    constructor(año, mes, dia) {
+        this.año = año;
+        this.mes = mes;
+        this.dia = dia;
+    }
+
+    diaDelAño() {
+        // TODO calcular el dia del año para la fecha
+    }
+
+    esBiciesto() {
+        // TODO returnar True si el año es biciesto
+    }
+
+}
---
+++ src/ea01_ADT/Punto2D.js
@@ -0,0 +1,13 @@
+class Punto2D {
+
+    distancia(p2) {
+        return Math.sqrt(
+            (this.getX - p2.getX) * (this.getX - p2.getX) + (this.getY - p2.getY) * (this.getY - p2.getY)
+        )
+    };
+
+    equals(p2) {
+        return this.distancia(p2) < 1E-6;
+    }
+
+}
